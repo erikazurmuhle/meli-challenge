3e8bb87823b895f0b6ec3007fc10d9029beb25f3
erikazurmuhle <erikadzurmuhle@gmail.com>
2021-09-24 11:43:23 -0300

    server config

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 3005;
+
+const morgan = require("morgan");
+const helmet = require("helmet");
+
+app.use(helmet());
+
+app.use(morgan("tiny"));
+// body parser
+app.use(express.urlencoded({ extended: true }));
+// json parser
+app.use(express.json());
+// routes of the app
+app.use("/api", require("./routes"));
+// error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+app.listen(3005, () => {
+  console.log("server on port 3005");
+});
