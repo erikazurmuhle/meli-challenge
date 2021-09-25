c7c655e0f1c0c5482541a5b342682765f386805a
erikazurmuhle <erikadzurmuhle@gmail.com>
2021-09-25 10:46:25 -0300

    css topProducts

--- frontend/src/BreadCrum.jsx
+++ frontend/src/BreadCrum.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./App.css";
 export default function BreadCrum() {
   return (
     <div className="breadcrum">
-      <h4>top &gt; top &gt; top &gt; </h4>
+      <Link>top &gt; top &gt; top &gt; </Link>
     </div>
   );
 }
